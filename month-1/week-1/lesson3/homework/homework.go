5a4129a4cabfcb9b065abe337c976e907c016bd2
Khumoyun <turabekov1110@gmail.com>
2023-01-29 10:34:14 +0500

    hometask functions

--- month-1/week-1/lesson3/homework/homework.go
+++ month-1/week-1/lesson3/homework/homework.go
@@ -5,11 +5,11 @@ import "fmt"
 func calc(num1, num2 int) (func() int, func() int) {
 
 	plus := func() int {
-		return num1 * 2
+		return num1 + num2
 	}
 
 	minus := func() int {
-		return num2 * 2
+		return num1 - num2
 	}
 
 	return plus, minus
@@ -17,7 +17,7 @@ func calc(num1, num2 int) (func() int, func() int) {
 
 func main() {
 
-	plus, minus := calc(20, 10)
+	plus, minus := calc(30, 10)
 
 	fmt.Println(plus())  // 40
 	fmt.Println(minus()) // 20
